fa39ac26eca46d16de8f0e9fa5ea23b64b5c34ae
Mark <kazhdan.mark@gmail.com>
2024-10-28 23:14:12 +0200

    FIX some methods

--- src/main/java/telran/employees/CompanyItems.java
+++ src/main/java/telran/employees/CompanyItems.java
@@ -2,9 +2,10 @@ package telran.employees;
 
 import telran.view.*;
 import java.util.*;
-import java.util.function.*;
 
 public class CompanyItems {
+    static InputOutput io = new StandardInputOutput();
+
     public static final long MIN_ID = 1;
     public static final long MAX_ID = 9999;
     public static final int MIN_BASIC_SALARY = 1000;
@@ -14,8 +15,8 @@ public class CompanyItems {
     public static final int MAX_WAGE = 100;
     public static final int MIN_HOURS = 1;
     public static final int MAX_HOURS = 160;
-    public static final float MIN_PERCENT = 1;
-    public static final float MAX_PERCENT = 100;
+    public static final float MIN_PERCENT = 1.0f;
+    public static final float MAX_PERCENT = 100.0f;
     public static final long MIN_SALES = 1000;
     public static final long MAX_SALES = 1000000;
     public static final float MIN_FACTOR = 1.0f;
@@ -23,142 +24,119 @@ public class CompanyItems {
 
     public static Item[] getItems(Company company) {
         List<Item> items = new ArrayList<>();
+        new Menu("Add Employee", hireMenu(company)).perform(io);
+        return items.toArray(new Item[0]);
+    }
 
-        items.add(Item.of("Add Employee", io -> {
-            Item[] hireMenu = getHireMenu(company);
-            int choice;
-            do {
-                io.writeLine("Select an option:");
-                for (int i = 0; i < hireMenu.length; i++) {
-                    io.writeLine((i + 1) + ". " + hireMenu[i].displayName());
-                }
-                io.writeLine("0. Go back");
-    
-                choice = validator(io, "Enter your choice:", "Invalid choice", 0, hireMenu.length, Integer::parseInt);
-    
-                if (choice > 0 && choice <= hireMenu.length) {
-                    hireMenu[choice - 1].perform(io); 
-                }
-            } while (choice != 0); 
+    private static Item[] hireMenu(Company company) {
+        List<Item> items = new ArrayList<>();
+        items.add(Item.of("Hire Employee", io -> {
+            Employee employee = standartEmployee(io);
+
+            company.addEmployee(new Employee(employee.getId(), employee.computeSalary(), employee.getDepartment()));
+            io.writeLine("Employee hired.");
         }));
-    
 
-        items.add(Item.of("Display Employee Data", io -> {
-            long id = io.readLong("Enter employee ID", "ID not found");
+        items.add(Item.of("Hire Wage Employee", io -> {
+            Employee employee = standartEmployee(io);
+
+            int wage = io.readNumberRange(
+                    String.format("Enter wage value in the range [%d-%d]", MIN_WAGE, MAX_WAGE),
+                    "Wrong Salary value", MIN_WAGE, MAX_WAGE).intValue();
+            int hours = io.readNumberRange(
+                    String.format("Enter hours value in the range [%d-%d]", MIN_HOURS, MAX_HOURS),
+                    "Wrong Salary value", MIN_HOURS, MAX_HOURS).intValue();
+            company.addEmployee(new WageEmployee(employee.getId(), employee.computeSalary(), employee.getDepartment(),
+                    wage, hours));
+            io.writeLine("Wage employee hired.");
+        }));
+
+        items.add(Item.of("Hire Sales Person", io -> {
+            Employee employee = standartEmployee(io);
+
+            long sales = io.readNumberRange(
+                    String.format("Enter sales value in the range [%d-%d]", MIN_SALES, MAX_SALES),
+                    "Wrong sales value", MIN_SALES, MAX_SALES).longValue();
+            float percent = io.readNumberRange(
+                    String.format("Enter percent value in the range [%.2f-%.2f]", MIN_PERCENT, MAX_PERCENT),
+                    "Wrong percent value", MIN_PERCENT, MAX_PERCENT).floatValue();
+            int wage = io.readNumberRange(
+                    String.format("Enter wage value in the range [%d-%d]", MIN_WAGE, MAX_WAGE),
+                    "Wrong wage value", MIN_WAGE, MAX_WAGE).intValue();
+            int hours = io.readNumberRange(
+                    String.format("Enter hours value in the range [%d-%d]", MIN_HOURS, MAX_HOURS),
+                    "Wrong hours value", MIN_HOURS, MAX_HOURS).intValue();
+            company.addEmployee(new SalesPerson(employee.getId(), employee.computeSalary(), employee.getDepartment(),
+                    wage, hours, percent, sales));
+            io.writeLine("Sales person hired.");
+        }));
+
+        items.add(Item.of("Hire Manager", io -> {
+            Employee employee = standartEmployee(io);
+
+            float factor = io.readNumberRange(
+                    String.format("Enter factor value in the range [%.2f-%.2f]", MIN_FACTOR, MAX_FACTOR),
+                    "Wrong factor value", MIN_FACTOR, MAX_FACTOR).floatValue();
+            company.addEmployee(
+                    new Manager(employee.getId(), employee.computeSalary(), employee.getDepartment(), factor));
+            io.writeLine("Manager hired.");
+        }));
+
+        items.add(Item.of("Display Employee Data", input -> {
+            long id = input.readLong("Enter employee ID", "ID not found");
             Employee employee = company.getEmployee(id);
             if (employee != null) {
-                io.writeLine(employee.toString());
+                input.writeLine(employee.toString());
             } else {
-                io.writeLine("Employee not found.");
+                input.writeLine("Employee not found.");
             }
         }));
 
-        items.add(Item.of("Fire Employee", io -> {
-            long id = io.readLong("Enter employee ID to fire", "ID not found");
+        items.add(Item.of("Fire Employee", input -> {
+            long id = input.readLong("Enter employee ID to fire", "ID not found");
             company.removeEmployee(id);
-            io.writeLine("Employee " + id + " fired.");
+            input.writeLine("Employee " + id + " fired.");
         }));
 
-        items.add(Item.of("Department Salary Budget", io -> {
-            String department = io.readString("Enter department");
+        items.add(Item.of("Department Salary Budget", input -> {
+            String department = input.readString("Enter department");
             double budget = company.getDepartmentBudget(department);
-            io.writeLine("Salary budget for department " + department + ": " + budget);
+            input.writeLine("Salary budget for department " + department + ": " + budget);
         }));
 
-        items.add(Item.of("List of Departments", io -> {
+        items.add(Item.of("List of Departments", input -> {
             String[] departments = company.getDepartments();
-            io.writeLine("Departments: " + String.join(", ", departments));
+            input.writeLine("Departments: " + String.join(", ", departments));
         }));
 
-        items.add(Item.of("Display Managers with Most Factor", io -> {
+        items.add(Item.of("Display Managers with Most Factor", input -> {
             Manager[] managers = company.getManagersWithMostFactor();
             for (Manager manager : managers) {
-                io.writeLine("Manager with ID " + manager.getId() +" ,factor: " + manager.getFactor());
+                input.writeLine("Manager with ID " + manager.getId() + " ,factor: " + manager.getFactor());
             }
         }));
 
-        items.add(Item.ofExit());
+        items.add(Item.of("Exit", io -> {
+            io.writeLine("Exiting program...");
+            System.exit(0);
+        }));
 
         return items.toArray(new Item[0]);
     }
 
-    private static Item[] getHireMenu(Company company) {
-        List<Item> items = new ArrayList<>();
-            items.add(Item.of("Hire Employee", io -> {
-                    long id = validator(io, "Enter ID:", "Wrong ID", MIN_ID, MAX_ID, Long::parseLong);
-                    int basicSalary = validator(io, "Enter Salary:", "Wrong value", MIN_BASIC_SALARY, MAX_BASIC_SALARY,
-                            Integer::parseInt);
-                    String department = departmentValidator(io, "Enter Department:", "Wrong department", DEPARTMENTS);
-                    company.addEmployee(new Employee(id, basicSalary, department));
-                    io.writeLine("Employee hired.");
-                }));
+    private static Employee standartEmployee(InputOutput io) {
+        HashSet<String> departmentsSet = new HashSet<>(List.of(DEPARTMENTS));
 
-                items.add(Item.of("Hire Wage Employee", io -> {
-                    long id = validator(io, "Enter ID:", "Wrong ID", MIN_ID, MAX_ID, Long::parseLong);
-                    int basicSalary = validator(io, "Enter Salary:", "Wrong value", MIN_BASIC_SALARY, MAX_BASIC_SALARY,
-                            Integer::parseInt);
-                    int wage = validator(io, "Enter Wage:", "Wrong Wage", MIN_WAGE, MAX_WAGE, Integer::parseInt);
-                    int hours = validator(io, "Enter Hours:", "Wrong Hours", MIN_HOURS, MAX_HOURS, Integer::parseInt);
-                    String department = departmentValidator(io, "Enter Department:", "Wrong department", DEPARTMENTS);
-                    company.addEmployee(new WageEmployee(id, basicSalary, department, wage, hours));
-                    io.writeLine("Wage employee hired.");
-                }));
+        long id = io.readNumberRange(String.format("Enter ID value in the range [%d-%d]", MIN_ID, MAX_ID),
+                "Wrong ID value", MIN_ID, MAX_ID).longValue();
+        int basicSalary = io.readNumberRange(
+                String.format("Enter Salary value in the range [%d-%d]", MIN_BASIC_SALARY, MAX_BASIC_SALARY),
+                "Wrong Salary value", MIN_BASIC_SALARY, MAX_BASIC_SALARY).intValue();
 
-                items.add(Item.of("Hire Sales Person", io -> {
-                    long id = validator(io, "Enter ID:", "Wrong ID", MIN_ID, MAX_ID, Long::parseLong);
-                    int basicSalary = validator(io, "Enter Salary:", "Wrong value", MIN_BASIC_SALARY, MAX_BASIC_SALARY,
-                            Integer::parseInt);
-                    long sales = validator(io, "Enter Sales:", "Wrong Sales", MIN_SALES, MAX_SALES, Long::parseLong);
-                    float percent = validator(io, "Enter Percent:", "Wrong Percent", MIN_PERCENT, MAX_PERCENT,
-                            Float::parseFloat);
-                    String department = departmentValidator(io, "Enter Department:", "Wrong department", DEPARTMENTS);
-                    int wage = validator(io, "Enter Wage:", "Wrong Wage", MIN_WAGE, MAX_WAGE, Integer::parseInt);
-                    int hours = validator(io, "Enter Hours:", "Wrong Hours", MIN_HOURS, MAX_HOURS, Integer::parseInt);
-                    company.addEmployee(new SalesPerson(id, basicSalary, department, wage, hours, percent, sales));
-                    io.writeLine("Sales person hired.");
-                }));
+        String department = io.readStringOptions("Enter department from " + departmentsSet,
+                "Must be one out from " + departmentsSet,
+                departmentsSet);
 
-                items.add(Item.of("Hire Manager", io -> {
-                    long id = validator(io, "Enter ID:", "Wrong ID", MIN_ID, MAX_ID, Long::parseLong);
-                    int basicSalary = validator(io, "Enter Salary:", "Wrong value", MIN_BASIC_SALARY, MAX_BASIC_SALARY,
-                            Integer::parseInt);
-                    String department = departmentValidator(io, "Enter Department:", "Wrong department", DEPARTMENTS);
-                    float factor = validator(io, "Enter Factor:", "Wrong Factor", MIN_FACTOR, MAX_FACTOR,
-                            Float::parseFloat);
-                    company.addEmployee(new Manager(id, basicSalary, department, factor));
-                    io.writeLine("Manager hired.");
-                }));
-
-        return items.toArray(new Item[0]);
-    }
-
-    private static <T extends Number & Comparable<T>> T validator(
-            InputOutput io, String prompt, String errorPrompt, T min, T max, Function<String, T> parseFunction) {
-
-        T value;
-        do {
-            value = io.readObject(prompt, errorPrompt, parseFunction);
-            if (value.compareTo(min) < 0 || value.compareTo(max) > 0) {
-                io.writeLine("Value must be between " + min + " and " + max);
-            }
-        } while (value.compareTo(min) < 0 || value.compareTo(max) > 0);
-
-        return value;
-    }
-
-    private static String departmentValidator(
-            InputOutput io, String prompt, String errorPrompt, String[] DEPARTMENTS) {
-
-        String department;
-        boolean isValid;
-        do {
-            department = io.readString(prompt);
-            isValid = Arrays.asList(DEPARTMENTS).contains(department);
-            if (!isValid) {
-                io.writeLine("Choose one: QA, Development, Audit, Marketing");
-            }
-        } while (!isValid);
-
-        return department;
+        return new Employee(id, basicSalary, department);
     }
 }
